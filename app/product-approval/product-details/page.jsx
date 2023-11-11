9709433a1327ef8be5d12929b2f115f07bfc40b3
Abhey Gupta <abheygupta1731@gmail.com>
2023-11-11 21:24:19 +0530

    intial commit 2

--- app/product-approval/product-details/page.jsx
+++ app/product-approval/product-details/page.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
-import OvalLoader from "@/components/admin/utility/OvalLoader";
+import OvalLoader from "@/components/utility/OvalLoader";
 import { toast } from "react-toastify";
 
 const ProductDetails = () => {
