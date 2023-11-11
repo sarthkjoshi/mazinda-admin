8f2a7b78cb63591649d199a244a79a3f965366cd
Abhey Gupta <abheygupta1731@gmail.com>
2023-11-12 04:25:14 +0530

    test

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import './globals.css'
 import { Quicksand } from 'next/font/google'
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
-import AuthWrapper from '@/components/auth/AuthWrapper';
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -26,29 +25,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={quicksand.className}>
-        <AuthWrapper>
-          <ToastContainer
-            position="top-center"
-            autoClose={3000}
-            hideProgressBar
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            transition={Slide}
-            theme="light"
-          />
-          <Navbar />
-          <NextTopLoader color="#F17E13" showSpinner={false} />
-          <div className="flex">
-            <div className='hidden md:block'>
-              <Sidebar />
-            </div>
-            <div className="flex-1 p-4 bg-gray-50">{children}</div>
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          transition={Slide}
+          theme="light"
+        />
+        <Navbar />
+        <NextTopLoader color="#F17E13" showSpinner={false} />
+        <div className="flex">
+          <div className='hidden md:block'>
+            <Sidebar />
           </div>
-        </AuthWrapper>
+          <div className="flex-1 p-4 bg-gray-50">{children}</div>
+        </div>
       </body>
     </html>
   )
